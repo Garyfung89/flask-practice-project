1707bf2fd632a9f497a8d6a04e0351ca0b0fb2e9
Gary FUNG <80943824+Garyfung89@users.noreply.github.com>
2025-07-29 22:50:18 +1000

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 '''
 # Import Flask, render_template, request from the flask pramework package
 # Import the sentiment_analyzer function from the package created
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app
@@ -22,8 +22,10 @@ def sent_analyzer():
 
     label = response['label']
     score = response['score']
-
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is None:
+        return "Invalid input! Try again."
+    result = label.split('_')[1]
+    return f"The given text has been identified as {result} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
@@ -33,6 +35,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
